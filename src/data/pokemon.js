76c1992827bc95269ec7018e815eb9eedfa9bddc
Randy Setiawan <laurensiusrandy17@gmail.com>
2026-06-19 14:01:14 +0700

    initial commit

---
+++ src/data/pokemon.js
@@ -0,0 +1,74 @@
+export const pokemons = [
+  {
+    id: 1,
+    name: "Pikachu",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
+    type: "Electric",
+  },
+  {
+    id: 2,
+    name: "Bulbasaur",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+    type: "Grass",
+  },
+  {
+    id: 3,
+    name: "Ivysaur",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png",
+    type: "Grass",
+  },
+  {
+    id: 4,
+    name: "Venusaur",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
+    type: "Grass",
+  },
+  {
+    id: 5,
+    name: "Squirtle",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
+    type: "Water",
+  },
+  {
+    id: 6,
+    name: "Wartortle",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png",
+    type: "Water",
+  },
+  {
+    id: 7,
+    name: "Blastoise",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png",
+    type: "Water",
+  },
+  {
+    id: 8,
+    name: "Charmander",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+    type: "Fire",
+  },
+  {
+    id: 9,
+    name: "Charmeleon",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png",
+    type: "Fire",
+  },
+  {
+    id: 10,
+    name: "Charizard",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png",
+    type: "Fire",
+  },
+  {
+    id: 11,
+    name: "Caterpie",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png",
+    type: "Bug",
+  },
+  {
+    id: 12,
+    name: "Butterfree",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/12.png",
+    type: "Bug",
+  },
+];
